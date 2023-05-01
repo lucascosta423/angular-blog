b2a687b7386158c3cb1333d495751c3465d5f72a
Lucas Costa <lukascosta.ls17@gmail.com>
2023-05-01 18:36:40 -0300

    update components

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./small-card.component.css']
 })
 export class SmallCardComponent {
+  @Input()
+  photocover:string = "";
+  @Input()
+  cardtitle:string = "";
 
+  constructor(){}
+
+  ngOnInit(): void{
+  }
 }
